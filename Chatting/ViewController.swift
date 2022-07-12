2b5f37fde414f8ea290aa65ab6f7d0e5ad48ca4f
minaje0917 <k050918@naver.com>
2022-07-12 11:19:10 +0900

    Initial Commit

---
+++ Chatting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chatting
+//
+//  Created by 선민재 on 2022/07/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
