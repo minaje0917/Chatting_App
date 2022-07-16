6c893f43870781b28f73784e69f58a93d6e45620
minaje0917 <k050918@naver.com>
2022-07-16 17:03:54 +0900

    :art: POP 👉 present

--- Chatting/Sources/VCs/nickNameViewController.swift
+++ Chatting/Sources/VCs/nickNameViewController.swift
@@ -59,10 +59,18 @@ class nickNameViewController: UIViewController {
     
     @objc func enterAction() {
         if isFilled(nickNameField) {
-            let lvc = chattingViewController()
-            lvc.modalPresentationStyle = .fullScreen
-            self.navigationController?.pushViewController(lvc, animated: true)
+            let vc = chattingViewController()
+            let transition = CATransition()
+            transition.duration = 0.5
+            transition.type = CATransitionType.push
+            transition.subtype = CATransitionSubtype.fromRight
+            transition.timingFunction =
+            CAMediaTimingFunction(name:CAMediaTimingFunctionName.easeInEaseOut)
+            view.window!.layer.add(transition, forKey: kCATransition)
+            vc.modalPresentationStyle = .fullScreen
+            self.present(vc, animated: false, completion: nil)
         }
+        
         else {
             nickNameField.layer.borderWidth = 2
             nickNameField.layer.borderColor = UIColor.wrong?.cgColor
