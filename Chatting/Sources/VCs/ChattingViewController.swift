43d0c8d5468dbef78c61b3a1c5bc3411e724e2a6
minaje0917 <k050918@naver.com>
2022-07-18 00:03:16 +0900

    Navigation Bar

--- Chatting/Sources/VCs/ChattingViewController.swift
+++ Chatting/Sources/VCs/ChattingViewController.swift
@@ -9,14 +9,23 @@ import UIKit
 import Then
 import SnapKit
 
-class chattingViewController: UIViewController {
+class ChattingViewController: UIViewController {
+    private let navigationTitleLabel = UILabel().then {
+        $0.text = "Who Are You"
+        $0.font = .systemFont(ofSize: 22, weight: .medium)
+        $0.textColor = .black
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .backGround
+        configNavigation()
     }
-    
-        
     
 }
 
+private extension ChattingViewController {
+    func configNavigation() {
+        self.navigationItem.titleView = navigationTitleLabel
+    }
+}
