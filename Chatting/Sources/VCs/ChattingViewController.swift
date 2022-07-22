fafca814c33fe11d8ed350eb440e06b9355c6f2c
minaje0917 <k050918@naver.com>
2022-07-22 16:25:02 +0900

    :sparkles: barButton customView

--- Chatting/Sources/VCs/ChattingViewController.swift
+++ Chatting/Sources/VCs/ChattingViewController.swift
@@ -10,20 +10,42 @@ import Then
 import SnapKit
 
 final class ChattingViewController: UIViewController {
+    private let bounds = UIScreen.main.bounds
+    
     private let navigationTitleLabel = UILabel().then {
         $0.text = "Who Are You"
         $0.font = UIFont(name: "NotoSansKR-Medium", size: 22)
         $0.textColor = .black
+    }
+    private let navigationBarButton = UIButton().then {
+        let text = NSAttributedString(string: "퇴장")
+        $0.setAttributedTitle(text, for: .normal)
+        $0.titleLabel?.font = UIFont(name: "NotoSansKR-Medium", size: 14)
+        $0.setTitleColor(UIColor.white, for: .normal)
+        $0.layer.cornerRadius = 5
+        $0.backgroundColor = .wrong
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .backGround
         configNavigation()
+        addView()
+        setLayout()
+
     }
     
-    @objc func buttonTappedAction() {
-        
+    private func addView() {
+        [navigationBarButton].forEach {
+            view.addSubview($0)
+        }
+    }
+    
+    private func setLayout() {
+        navigationBarButton.snp.makeConstraints {
+            $0.width.equalTo(70)
+            $0.height.equalTo(30)
+        }
     }
     
 }
@@ -31,11 +53,8 @@ final class ChattingViewController: UIViewController {
 private extension ChattingViewController {
     func configNavigation() {
         self.navigationItem.titleView = navigationTitleLabel
-        navigationItem.rightBarButtonItem = UIBarButtonItem(
-        title: "나가기",
-        style: .plain,
-        target: self,
-        action: #selector(buttonTappedAction)
-        )
+        let rightBarButton = UIBarButtonItem(customView: navigationBarButton)
+        self.navigationItem.rightBarButtonItem = rightBarButton
+        
     }
 }
