b6d83156a461b7feb9acbd908649f564ae862e2d
minaje0917 <k050918@naver.com>
2022-07-28 16:06:57 +0900

    :sparkles: add sentButton and keyboard action

--- Chatting/Sources/VCs/ChattingViewController.swift
+++ Chatting/Sources/VCs/ChattingViewController.swift
@@ -12,6 +12,38 @@ import SnapKit
 final class ChattingViewController: UIViewController {
     private let bounds = UIScreen.main.bounds
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func addKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func removeKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+    }
+    
+    
+    @objc func keyboardWillShow(_ noti: NSNotification){
+        if let keyboardFrame: NSValue = noti.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue{
+            let keyboardRectangle = keyboardFrame.cgRectValue
+            let keyboardHeight = keyboardRectangle.height
+            self.view.frame.origin.y -= keyboardHeight
+        }
+    }
+    
+    @objc func keyboardWillHide(_ noti: NSNotification){
+        if let keyboardFrame: NSValue = noti.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue{
+            let keyboardRectangle = keyboardFrame.cgRectValue
+            let keyboardHeight = keyboardRectangle.height
+            self.view.frame.origin.y += keyboardHeight
+        }
+    }
+    
     private let navigationTitleLabel = UILabel().then {
         $0.text = "Who Are You"
         $0.font = UIFont(name: "NotoSansKR-Medium", size: 22)
@@ -25,6 +57,10 @@ final class ChattingViewController: UIViewController {
     private let chattingField = UITextField().then {
         $0.backgroundColor = .white
         $0.layer.cornerRadius = 16
+    }
+    
+    private let sentButton = UIButton().then {
+        $0.setImage(UIImage(named: "sentButton"), for: .normal)
     }
     
     private let navigationBarButton = UIButton().then {
@@ -42,11 +78,17 @@ final class ChattingViewController: UIViewController {
         configNavigation()
         addView()
         setLayout()
-
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.addKeyboardNotifications()
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        self.removeKeyboardNotifications()
     }
     
     private func addView() {
-        [navigationBarButton,backView,chattingField].forEach {
+        [navigationBarButton,backView,chattingField,sentButton].forEach {
             view.addSubview($0)
         }
     }
@@ -64,10 +106,14 @@ final class ChattingViewController: UIViewController {
         }
         chattingField.snp.makeConstraints {
             $0.centerX.equalToSuperview()
-            $0.bottom.equalTo(backView.snp.bottom).offset(-36 )
+            $0.bottom.equalTo(backView.snp.bottom).offset(-36)
             $0.trailing.equalToSuperview().offset(-59)
             $0.leading.equalToSuperview().offset(16)
             $0.height.equalTo(30)
+        }
+        sentButton.snp.makeConstraints {
+            $0.trailing.equalTo(chattingField.snp.trailing).offset(39)
+            $0.bottom.equalTo(backView.snp.bottom).offset(-36)
         }
     }
     
